5cb50380d6b2a17019f77b7152159f484d8006be
IfeanyichukwuPeter <mbaifeanyichukwu70@gmail.com>
2026-02-13 12:46:57 +0100

    Nav and footer fetch with menu initialization
    Added initMenu function to handle menu interactions and update menu icon.

--- bit.js
+++ bit.js
@@ -1,12 +1,46 @@
-fetch ("components/nav.html")
-    .then(res=> res.text())
-    .then(data => {
-        document.getElementById("nav").innerHTML = data;
-    });
+fetch("components/nav.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("nav").innerHTML = data;
+    initMenu();
+  });
 
-fetch ("components/footer.html")
-      .then(res => res.text())
-      .then(data => {
-        document.getElementById("footer").innerHTML = data;
-      });
-      
+fetch("components/footer.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+
+function initMenu() {
+  const menu = document.getElementById("menu");
+  const oldBtn = document.getElementById("menu-btn");
+
+  if (!menu || !oldBtn) return;
+
+  const menuBtn = oldBtn.cloneNode(true);
+  oldBtn.parentNode.replaceChild(menuBtn, oldBtn);
+
+  const menuIcon = menuBtn.querySelector("#burger");
+
+  menuBtn.addEventListener("click", () => {
+    console.log("clicked");
+
+    menu.classList.toggle("active");
+    const isOpen = menu.classList.contains("active");
+
+   menuIcon.style.opacity = "0";
+
+setTimeout(() => {
+  menuIcon.src = isOpen
+    ? "/bitimg/icon-close.svg"
+    : "/bitimg/icon-hamburger.svg";
+
+  menuIcon.style.opacity = "1";
+}, 150);
+menuIcon.style.transform = isOpen
+  ? "rotate(90deg)"
+  : "rotate(0deg)";
+
+  });
+}
